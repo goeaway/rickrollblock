c5ef6e13654b43a6e40e0c9352b9453bec22ad76
Joe Thompson <joseph.thompson>
2020-12-02 19:03:27 +0000

    styled extension page

--- src/content/components/content-app.tsx
+++ src/content/components/content-app.tsx
@@ -1,5 +1,5 @@
 import React, { FC } from "react";
-import styled from "styled-components";
+import styled, { css } from "styled-components";
 import { browser } from "webextension-polyfill-ts";
 
 const ContentApp : FC = () => {
@@ -13,35 +13,97 @@ const ContentApp : FC = () => {
 
     return (
         <Container>
-            <Title>
-                That was close!
-            </Title>
-            <Explanation>
-                It looks like someone tried to Rick Roll you. Luckily we stopped it.
-            </Explanation>
-            <Button onClick={allowOnceHandler}>Rick Roll me just this once</Button>
-            <Button onClick={unblockUrlHandler}>Unblock this URL</Button>
+            <Hero>
+                <TitleContainer>
+                    <Name>
+                        Rick Roll Blocker
+                    </Name>
+                    <Title>
+                        That was close!
+                    </Title>
+                </TitleContainer>
+                <Explanation>
+                    It looks like someone tried to Rick Roll you, luckily we stopped it!
+                </Explanation>
+                <ButtonRow>
+                    <Button onClick={allowOnceHandler}>Rick Roll me just this once</Button>
+                    <Button variant="secondary" onClick={unblockUrlHandler}>Unblock this URL</Button>
+                </ButtonRow>
+            </Hero>
         </Container>
     );
 }
 
 export default ContentApp;
 
-const Container = styled.div`
+const Hero = styled.div`
     display: flex;
     flex-direction: column;
-    align-items: flex-start;
+    align-items: center;
+    justify-content: center;
+    height: 100vh;
+    gap: 1rem;
+    color: rgba(0,0,0,0.87);
+    background: #F9FAFB;
+    padding: 2rem;
+`
+
+const TitleContainer = styled.div`
+    display: flex;
+    flex-direction: column;
 `
 
 const Title = styled.span`
     font-size: 100px;
-    line-height: 120px;
+    line-height: 100px;
 `
 
 const Explanation = styled.span`
 
 `;
 
+interface ButtonProps {
+    variant?: "primary" | "secondary";
+}
+
 const Button = styled.button`
+    border-radius: 4px;
+    outline: none;
+    padding: .5rem 1rem;
+    cursor: pointer;
+    font-family: inherit;
+    color: #F9FAFB;
+    background: #111827;
+    border: 1px solid #111827;
+    transition: background 300ms ease;
+
+    &:hover {
+        background: #374151;
+    }
+
+    ${(p: ButtonProps) => p.variant === "secondary" && css`
+        border: 1px solid #4B5563;
+        color: #111827;
+        background: #F9FAFB;
+
+        &:hover {
+            background: #E5E7EB;
+        }
+    
+    `}
+`
+
+const ButtonRow = styled.div`
+    display: flex;
+    gap: 1rem;
+`
+
+const Name = styled.span`
+    font-size: 18px;
+    line-height: 22px;
+    font-weight: 600;
+`
+
+const Container = styled.div`
 
 `
